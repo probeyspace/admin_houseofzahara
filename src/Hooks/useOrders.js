c97e5c43060a5b4fd94367f8e0c8c90a232ec00d
uvaishnasir <uvaishnasir@gmail.com>
2025-08-18 16:36:13 +0530

    use order fix

--- src/Hooks/useOrders.js
+++ src/Hooks/useOrders.js
@@ -1,19 +1,14 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import api from "../Api/api";
 import { setOrders } from "../store/slices/orderSlice";
 
 const useOrders = () => {
   const dispatch = useDispatch();
-  const user = useSelector((store) => store.user);
-  const isAdmin = user?.role === "ADMIN";
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        //check role and fetch products accordingly if admin then fetch all products else fetch only vendor products
-        let endpoint = isAdmin ? "/orders/all" : "/orders/vendor";
-        const response = await api.get(endpoint);
-        // console.log(response.data.data); 
+        const response = await api.get("/orders");
         dispatch(setOrders(response.data.data));
       } catch (error) {
         console.error("Error fetching orders:", error);
